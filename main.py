89c4f91ed4fd0a407724eafadc8abb331e29221e
Victor <vildanpro@gmail.com>
2023-08-14 15:42:00 +0500

    add requirements

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from db import Session, OcFilecacheModel
 
 
+filter_string = '%groupfolders/trash/28/%'
+
 with Session() as session:
-    filtered_rows = session.query(OcFilecacheModel).filter(OcFilecacheModel.path.like('%groupfolders/trash/28/%')).all()
+    filtered_rows = session.query(OcFilecacheModel).filter(OcFilecacheModel.path.like(filter_string)).all()
 
 print(filtered_rows)
